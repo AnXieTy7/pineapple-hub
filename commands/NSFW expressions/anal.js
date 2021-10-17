ae3460eb4984083332474049d8d81d7874713414
AnXieTy7 <36940701+AnXieTy7@users.noreply.github.com>
2021-10-17 14:33:30 -0400

    Add files via upload

---
+++ commands/NSFW expressions/anal.js
@@ -0,0 +1,11 @@
+const client = require('nekos.life');
+const Discord = require('discord.js')
+const neko = new client();
+disbut = require("discord-buttons");
+module.exports = {
+  name: "anal",
+  category: "NSFW expressions",
+  usage: "[command]",
+  run: async (client, message, args) => {
+  }
+                };
---
+++ commands/NSFW/neko.js
@@ -0,0 +1,11 @@
+const client = require('nekos.life');
+const Discord = require('discord.js')
+const neko = new client();
+disbut = require("discord-buttons");
+module.exports = {
+  name: "neko",
+  category: "NSFW",
+  usage: "[command]",
+  run: async (client, message, args) => {
+  }
+                };
